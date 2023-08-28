4c30c8e536378e6cc00d2cbedd6a63cac01b919a
Oren Yaniv <51949130+Lizon57@users.noreply.github.com>
2023-08-29 01:14:59 +0300

    [code-style] Initial AppHeader (no functionallity). Initial style system

---
+++ src/components/layout/app-header.tsx
@@ -0,0 +1,17 @@
+import styles from './app-header.module.scss'
+import { BiDownload, BiUpload } from 'react-icons/bi'
+
+export function AppHeader() {
+    return (
+        <header className={styles.container}>
+            <div className={styles.content}>
+                <input type="search" placeholder="Type to search photos..." title="Type search term" />
+
+                <div className={styles.actions_container}>
+                    <button title="Save results"><BiDownload />Save</button>
+                    <button title="Browse saved results"><BiUpload />Restore</button>
+                </div>
+            </div>
+        </header>
+    )
+}
