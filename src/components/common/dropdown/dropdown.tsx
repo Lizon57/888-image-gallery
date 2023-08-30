757a2ed9a34d887b32ca55654f69db808dddaf81
Oren Yaniv <51949130+Lizon57@users.noreply.github.com>
2023-08-30 10:15:50 +0300

    [code-style] Implement restore search feature. create controller (input, button) primary mixin

---
+++ src/components/common/dropdown/dropdown.tsx
@@ -0,0 +1,52 @@
+import { ReactElement, useRef } from 'react'
+import uuid from 'react-uuid'
+import { useOnClickOutside } from '@/hooks/use-onclick-outside'
+import styles from './dropdown.module.scss'
+
+
+export function Dropdown({ controller, items }: Props) {
+    const checkboxRef = useRef<HTMLInputElement>(null)
+
+    const closeDropdownContent = () => {
+        if (!checkboxRef.current!.checked) return
+        checkboxRef.current!.checked = false
+    }
+
+    useOnClickOutside(checkboxRef, closeDropdownContent)
+
+
+    const onItemClick = (callback: () => void) => {
+        callback()
+        closeDropdownContent()
+    }
+
+
+    return (
+        <div className={styles.container}>
+            <input type="checkbox" className={styles.checkbox} ref={checkboxRef} />
+            <button title={controller.title} className={styles.controller}>{controller.display}</button>
+
+            <div className={styles.content_container}>
+                {items.map(item => <button
+                    key={uuid()}
+                    className={styles.item}
+                    onClick={() => onItemClick(item.onClick)}
+                >
+                    {item.title}
+                </button>)}
+            </div>
+        </div>
+    )
+}
+
+
+type Props = {
+    controller: {
+        title: string
+        display: ReactElement | string
+    }
+    items: {
+        title: string,
+        onClick: () => void
+    }[]
+}
--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -3,11 +3,21 @@ import { BiDownload, BiUpload } from 'react-icons/bi'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store/store'
 import { setSearchTerm } from '@/store/actions/app.action'
+import { localStorageService } from '@/services/local-storage.service'
+import { GALLERY_CACHE_KEY } from '@/constants/gallery-cache-key'
+import { Dropdown } from '@/components/common/dropdown/dropdown'
 import styles from './app-header.module.scss'
 
 
 export function AppHeader() {
     const { searchTerm } = useSelector((state: RootState) => state.appModule)
+
+    const searchHistory = Object.keys(localStorageService.read(GALLERY_CACHE_KEY) || {})
+    const formattedSearchHistory = searchHistory.map(key => ({
+        title: key,
+        onClick: () => setSearchTerm(key)
+    }))
+
 
     const onInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
         const { value } = ev.target
@@ -24,13 +34,20 @@ export function AppHeader() {
                     title="Type search term"
                     autoFocus
                     onChange={onInputChange}
+                    value={searchTerm}
                 />
 
                 <div className={styles.actions_container}>
                     {searchTerm && <button title="Save results"><BiDownload />Save</button>}
-                    <button title="Browse saved results"><BiUpload />Restore</button>
+                    {!!searchHistory.length && <Dropdown
+                        controller={{
+                            title: 'Browse saved results',
+                            display: <><BiUpload />Restore</>
+                        }}
+                        items={formattedSearchHistory}
+                    />}
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
